106d25806775ea43fc752ce3bcd6eff2b066c641
brianmstricker <brianmstricker@gmail.com>
2023-09-27 14:45:38 -0400

    place filter design

--- src/components/placefilter/FilterItem.tsx
+++ src/components/placefilter/FilterItem.tsx
@@ -5,10 +5,10 @@ type FilterItemProps = {
 
 const FilterItem = ({ icon, text }: FilterItemProps) => {
  return (
-  <div className="flex flex-col items-center gap-2 relative min-w-[56px] px-[3.1rem]">
-   <div className="text-2xl">{icon}</div>
+  <div className="flex flex-col items-center gap-1 relative min-w-[56px] px-[3.15rem]">
+   <div className="text-[28px]">{icon}</div>
    <div className="whitespace-nowrap relative">
-    <span className="font-medium tracking-tight">{text}</span>
+    <span className="font-medium tracking-tight text-[12px]">{text}</span>
    </div>
   </div>
  );
--- src/components/placefilter/PlaceFilter.tsx
+++ src/components/placefilter/PlaceFilter.tsx
@@ -16,12 +16,64 @@ import {
 import { AiOutlinePicture } from "react-icons/ai";
 import { TbAdjustmentsHorizontal, TbBeach, TbUfo } from "react-icons/tb";
 import { CgGames } from "react-icons/cg";
-import { FaChevronRight, FaSkiing } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight, FaSkiing } from "react-icons/fa";
+import { useEffect, useRef, useState } from "react";
 
 const PlaceFilter = () => {
+ const sliderRef = useRef(null);
+ const [sliderPosition, setSliderPosition] = useState(getSliderPosition());
+ const slideLeft = () => {
+  const slider = sliderRef.current;
+  slider ? (slider.scrollLeft -= 300) : null;
+ };
+ const slideRight = () => {
+  const slider = sliderRef.current;
+  slider ? (slider.scrollLeft += 300) : null;
+ };
+ function getSliderPosition() {
+  const slider = sliderRef.current;
+  if (slider) {
+   const sliderPosition = slider.scrollLeft;
+   return sliderPosition;
+  }
+  return 0;
+ }
+ useEffect(() => {
+  const slider = sliderRef.current;
+  function handleSliderScroll() {
+   setSliderPosition(getSliderPosition());
+  }
+  if (slider) {
+   slider.addEventListener("scroll", handleSliderScroll);
+  }
+  return () => {
+   if (slider) {
+    slider.removeEventListener("scroll", handleSliderScroll);
+   }
+  };
+ }, []);
+ const maxScroll = slider.scrollWidth - slider.clientWidth;
  return (
-  <div className="pt-3 pb-12 text-xs text-gray-500 flex justify-between items-center">
-   <div className="grid grid-flow-col overflow-clip relative">
+  <div className="pt-3 pb-12 text-xs text-gray-500 flex justify-between items-center relative">
+   <div
+    className={
+     "absolute w-12 h-14 bg-gradient-to-r from-white via-white/50 to-transparent top-2 left-[30px] z-10" +
+     (sliderPosition === 0 ? " hidden" : "")
+    }
+   />
+   <button
+    className={
+     "p-2 items-center border rounded-full border-gray-400 text-black flex" +
+     (sliderPosition === 0 ? " hidden" : "")
+    }
+   >
+    <FaChevronLeft onClick={slideLeft} />
+   </button>
+   <div
+    className="grid grid-flow-col overflow-x-scroll relative whitespace-nowrap scrollbar-hide scroll-smooth"
+    id="slider"
+    ref={sliderRef}
+   >
     <FilterItem icon={<HiOutlineHome />} text="Tiny homes" />
     <FilterItem icon={<PiCastleTurretBold />} text="Castles" />
     <FilterItem icon={<MdCabin />} text="Cabins" />
@@ -37,11 +89,16 @@ const PlaceFilter = () => {
     <FilterItem icon={<PiLighthouse />} text="Lighthouse" />
     <FilterItem icon={<FaSkiing />} text="Skiing" />
     <FilterItem icon={<GiCampingTent />} text="Camping" />
-    <div className="absolute w-9 h-9 bg-white/30 right-2 rounded-full bottom-1" />
    </div>
-   <div className="flex items-center gap-5">
-    <button className="p-2 flex items-center border rounded-full border-gray-400 text-black">
-     <FaChevronRight />
+   <div className="flex items-center gap-5 relative">
+    <div className="absolute w-4 h-14 bg-gradient-to-r from-transparent via-white/50 to-white top-0 -left-4" />
+    <button
+     className={
+      "p-2 flex items-center border rounded-full border-gray-400 text-black" +
+      (maxScroll - sliderPosition === 0 ? " opacity-40" : "")
+     }
+    >
+     <FaChevronRight onClick={slideRight} />
     </button>
     <button className="p-4 flex items-center text-black border rounded-xl border-gray-300 gap-2">
      <TbAdjustmentsHorizontal size={20} />
